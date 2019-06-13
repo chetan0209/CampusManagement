da39534fffa888f2bab0a8add922d0e107796c43
chetan0209 <chetanbhandari0209@gmail.com>
2019-06-13 23:25:16 +0530

    Add files via upload

---
+++ SpringBootLoggingApplication.java
@@ -0,0 +1,32 @@
+package com.javatechie.spring.logging.api;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringBootLoggingApplication {
+
+	Logger log = LoggerFactory.getLogger(SpringBootLoggingApplication.class);
+
+	@GetMapping("/test/{name}")
+	public String greeting(@PathVariable String name) {
+		log.info("Request {}", name);
+		if (name.equalsIgnoreCase("test")) {
+			log.error("ERROR has occurred................");
+			throw new RuntimeException("Opps Exception raised....");
+		}
+		String response = "Hi " + name + " Welcome to Java Techie";
+		log.info("Response {}", response);
+		return response;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLoggingApplication.class, args);
+	}
+}
